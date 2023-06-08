19598118c49f64d2f21d9cd49c46df006a11e259
Pete Wond <pete@alberon.co.uk>
2023-06-08 20:11:55 +0100

    start on parser

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"go_interpreter/ast"
+	"go_interpreter/lexer"
+	"go_interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	// todo
+	return nil
+}
